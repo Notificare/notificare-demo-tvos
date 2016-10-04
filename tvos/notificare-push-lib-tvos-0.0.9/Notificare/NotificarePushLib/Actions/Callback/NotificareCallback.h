067fb4a719fae7e84d77d8ffcaaca97b91775f2d
Joel Oliveira <joeloliveira1979@gmail.com>
2016-10-04 03:53:48 +0200

    updated lib

---
+++ tvos/notificare-push-lib-tvos-0.0.9/Notificare/NotificarePushLib/Actions/Callback/NotificareCallback.h
@@ -0,0 +1,39 @@
+//
+//  NotificareCallback.h
+//  NotificarePushLib
+//
+//  Created by Joel Oliveira on 2/3/13.
+//  Copyright (c) 2013 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import <MobileCoreServices/MobileCoreServices.h>
+#import "ActionType.h"
+#import "ActionDelegate.h"
+#import "NotificareAction.h"
+#import "NotificareNotification.h"
+#import "NSString+FromBundle.h"
+#import "UIImage+FromBundle.h"
+#import "NSDictionary+FromPlist.h"
+#import "NotificareNetworkHost.h"
+
+@interface NotificareCallback : NSObject <ActionType,UINavigationControllerDelegate,UITabBarDelegate,UITextFieldDelegate>
+
+@property (nonatomic, assign) id<ActionDelegate> delegate;
+@property (strong, nonatomic) NotificareAction * action;
+@property (strong, nonatomic) NotificareNotification * notification;
+@property (strong, nonatomic) UIViewController * rootViewController;
+@property (strong, nonatomic) UIBarButtonItem * sendButton;
+@property (strong, nonatomic) UIBarButtonItem * closeButton;
+@property (strong, nonatomic) NSString * message;
+@property (strong, nonatomic) UITextField * messageField;
+@property (nonatomic, assign) BOOL withMessage;
+@property (strong, nonatomic) UINavigationController *navController;
+@property (strong, nonatomic) UIViewController *controller;
+@property (strong, nonatomic) UIView *baseView;
+@property (strong, nonatomic) NotificareNetworkHost *notificareNetworkHost;
+@property (strong, nonatomic) UIViewController * originalView;
+
+-(void)executeAction;
+
+@end
---
+++ tvos/notificare-push-lib-tvos-0.0.9/Notificare/NotificarePushLib/Types/Video/NotificareVideo.h
@@ -0,0 +1,40 @@
+//
+//  NotificareVideo.m
+//  NotificarePushLib
+//
+//  Created by Joel Oliveira on 10/02/16.
+//  Copyright (c) 2016 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import "NotificareNotification.h"
+#import "NotificareAction.h"
+#import "NotificareContent.h"
+#import "NotificareAttachment.h"
+#import "NotificationType.h"
+#import "NotificationDelegate.h"
+#import "NotificareActions.h"
+#import "NSString+FromBundle.h"
+#import "UIImage+FromBundle.h"
+
+@interface NotificareVideo : NSObject <NotificationType, UITextViewDelegate>
+
+@property (nonatomic, assign) id<NotificationDelegate> delegate;
+@property (nonatomic, strong) NotificareNotification * notification;
+@property (nonatomic, strong) NotificareActions * notificareActions;
+@property (strong, nonatomic) UIViewController * rootViewController;
+@property (strong, nonatomic) UIViewController * viewController;
+@property (strong, nonatomic) UINavigationController * navigationController;
+@property (strong, nonatomic) UIView * baseView;
+@property (strong, nonatomic) AVPlayer * avPlayer;
+@property (strong, nonatomic) UIBarButtonItem * closeButton;
+@property (strong, nonatomic) UIBarButtonItem * actionsButton;
+@property (strong, nonatomic) UIAlertController *actionSheet;
+@property (strong, nonatomic) UIViewController * originalView;
+
+
+-(void)openNotification;
+-(void)sendData:(NSArray *)data;
+
+@end
